d0075c7f60705d4cb2f9815010d827bec3799206
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2021-09-08 09:49:58 +0500

    Added Simultaneous Handlers

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { View, Button, Text, StyleSheet } from 'react-native';
-import { PanGestureHandler, PinchGestureHandler, PinchGestureHandlerGestureEvent } from 'react-native-gesture-handler';
+import React, { useState, useRef } from 'react';
+import { View, Button, Text, StyleSheet, Dimensions } from 'react-native';
+import { PanGestureHandler, PanGestureHandlerGestureEvent, PinchGestureHandler, PinchGestureHandlerGestureEvent } from 'react-native-gesture-handler';
 import Animated, {
   useSharedValue,
   withTiming,
@@ -8,25 +8,48 @@ import Animated, {
   Easing,
   useAnimatedGestureHandler,
 } from 'react-native-reanimated';
-// import Svg, { Circle, Rect } from 'react-native-svg';
+import Svg, { Circle, Rect } from 'react-native-svg';
 
-{/* <Svg height="50%" width="50%" viewBox="0 0 100 100" >
-  <Circle cx="40" cy="50" r="20" fill="black" />
-</Svg> */}
+
+const Circles = () => {
+  return (
+    < Svg height="50%" width="50%" viewBox="0 0 100 100" >
+      <Circle cx="40" cy="50" r="1" fill="white" />
+    </Svg >
+
+  )
+}
+
 const SKETCH_WIDTH = 500
 const SKETCH_HEIGHT = 500
+const { width, height } = Dimensions.get('window');
+//x increases towards right and y towards down
 
-export default function AnimatedStyleUpdateExample() {
+export default function Sketch() {
 
+  const panRef = useRef()
+  const pinchRef = useRef();
+
+  const [isDrawings, setIsDrawings] = useState(false);
+
+  const isMoving = useSharedValue(false);
   const isZoom = useSharedValue(false);
   const isDrawing = useSharedValue(false);
   const scale = useSharedValue(1);
   // const focalX = useSharedValue(0);
   // const focalY = useSharedValue(0);
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+  const point = useSharedValue([]);
+
+  const x = useSharedValue(0);
+  const y = useSharedValue(0);
 
   const pinchHandler = useAnimatedGestureHandler<PinchGestureHandlerGestureEvent>({
     onStart: (e, ctx: any) => {
       ctx.scale = scale.value;
+      // ctx.focalX = focalX.value;
+      // ctx.focalY = focalY.value;
       if (e.numberOfPointers == 1) {
         isDrawing.value = true;
       } else if (e.numberOfPointers == 2) {
@@ -34,33 +57,54 @@ export default function AnimatedStyleUpdateExample() {
       }
     },
     onActive: (e, ctx: any) => {
-      scale.value = e.scale - (1-ctx.scale);
-      // focalX.value = e.focalX
-      // focalY.value = e.focalY
-      // console.log(e.focalX)
-      
-      // console.log("focalX", e.focalX)
+      scale.value = e.scale - (1 - ctx.scale);
+      // focalX.value = ctx.focalX + e.focalX;
+      // focalY.value = ctx.focalY + e.focalY;
+      // console.log('x',e.focalX, 'y',e.focalY)
     },
     onFinish: (e) => {
       isZoom.value = false;
       isDrawing.value = false;
-      // focalX.value = 0;
-      // focalY.value = 0;
+    }
+  })
+
+  const panHandler = useAnimatedGestureHandler<PanGestureHandlerGestureEvent>({
+    onStart: (e, ctx: any) => {
+      console.log('hi')
+      ctx.translateX = translateX.value
+      ctx.translateY = translateY.value
+      isDrawing.value = true;
+    },
+    onActive: (e, ctx: any) => {
+      if (isDrawings) {
+        x.value = e.x;
+        y.value = e.y;
+        point.value.push({ x: x.value, y: y.value })
+        return;
+      }
+      translateX.value = ctx.translateX + e.translationX;
+      translateY.value = ctx.translateY + e.translationY;
+      // console.log(e.x)
+    },
+    onFinish: (e) => {
+
     }
   })
 
   const style = useAnimatedStyle(() => {
     return {
       transform: [
-        // { translateX: focalX.value },
-        // { translateY: focalY.value },
-        // { translateX: -SKETCH_WIDTH / 2 },
-        // { translateY: -SKETCH_HEIGHT / 2 },
         { scale: scale.value },
-        // { translateX: -focalX.value },
-        // { translateY: -focalY.value },
-        // { translateX: SKETCH_WIDTH / 2 },
-        // { translateY: SKETCH_HEIGHT / 2 },
+      ]
+    }
+  })
+
+  const panStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: translateX.value },
+        { translateY: translateY.value },
+        { scale: scale.value }
       ]
     }
   })
@@ -69,14 +113,24 @@ export default function AnimatedStyleUpdateExample() {
     <View
       style={{
         flex: 1,
-        backgroundColor: 'black'
+        backgroundColor: 'black',
+        marginTop: 20,
+        alignItems: 'center'
       }}>
-      <View>
+      <Button title={isDrawings ? 'cancel drawing' : 'do drawing'} onPress={() => setIsDrawings(!isDrawings)} />
 
-        <PinchGestureHandler onGestureEvent={pinchHandler}>
-          <Animated.View style={[styles.paintContainer, style]} />
-        </PinchGestureHandler>
-      </View>
+      <PanGestureHandler onGestureEvent={panHandler} ref={panRef} simultaneousHandlers={pinchRef}>
+        <Animated.View>
+
+          <PinchGestureHandler onGestureEvent={pinchHandler} ref={pinchRef} simultaneousHandlers={panRef}>
+            <Animated.View style={[styles.paintContainer, panStyle]}>
+
+            </Animated.View>
+          </PinchGestureHandler>
+
+        </Animated.View>
+      </PanGestureHandler>
+
 
     </View>
   );
@@ -87,6 +141,6 @@ const styles = StyleSheet.create({
     // flex: 1,
     width: 500,
     height: 500,
-    backgroundColor: 'grey'
+    backgroundColor: 'grey',
   }
 })
